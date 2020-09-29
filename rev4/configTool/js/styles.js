cc961fd38bd2891d4b19269bdd0390d3c8f036de
Kanta HORIO <kanta.horio@gmail.com>
2020-09-29 12:16:34 +0900

    Config tool css update

--- rev4/configTool/js/styles.js
+++ rev4/configTool/js/styles.js
@@ -1,10 +1,16 @@
 // file-input
-window.pressed = function(){
-	var input = document.getElementById('file');
-	if(input.value == "") {
-		fileLabel.innerHTML = "Choose file";
-	} else {
-		var theSplit = input.value.split('\\');
-		fileLabel.innerHTML = theSplit[theSplit.length-1];
-	}
-};
+const formInputs = document.getElementsByClassName("file");
+const length = formInputs.length;
+for (let i = 0; i < length; i++) {
+    formInputs[i].onchange = function () {
+        const file = this.files[0].name;
+        const label = this.nextElementSibling;
+        if (!label.classList.contains("changed")) {
+            const span = document.createElement("span");
+            span.className = "filename";
+            this.parentNode.appendChild(span);
+            label.classList.add("changed");
+        }
+        label.nextElementSibling.innerHTML = file;
+    };
+}
